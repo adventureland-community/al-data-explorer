ea1cf1650a6ca78af29ebb33c949a1828a8dbb6a
thmsn <9084377+thmsndk@users.noreply.github.com>
2022-10-28 22:54:54 +0200

    data.json will now be formatted with prettier for easier git diffs

--- update-data.ts
+++ update-data.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { writeFileSync } from "fs";
 import { join } from "path";
+import prettier from "prettier";
 export function updateData() {
+  console.log("Downloading data.js");
+
   axios
     .get<string>("https://adventure.land/data.js")
     .then(function (response) {
@@ -10,12 +13,23 @@ export function updateData() {
       const data = js.substring(6, js.length - 1);
       const json = JSON.parse(data);
       json.timestamp = new Date()
+      console.log(`data.js v${json.version} fetched`);
 
-      writeFileSync(join(__dirname, "public/data.json"), JSON.stringify(json), { flag: "w" });
+      
+      const prettierOptions = { parser: "json" };
+
+      console.log(`data.js formatting`);
+      const formatted = prettier.format(JSON.stringify(json), prettierOptions);
+
+      console.log(`data.js writing`);
+      // writeFileSync(join(__dirname, "public/data.json"), 
+      // formatted, { flag: "w" });
+      writeFileSync(join(__dirname, "public/data.json"), 
+      formatted);
     })
     .catch(function (error) {
       // handle error
-      console.log(error);
+      console.error(error);
     })
     .then(function () {
       // always executed
