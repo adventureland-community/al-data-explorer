aa035d66d8e8c048679fc46d7eea8175660fe43f
Telokis <telokis0@gmail.com>
2022-11-22 23:28:14 +0100

    Remove "s" from otherStatTypes

--- src/GearPlanner/StatsPanel.tsx
+++ src/GearPlanner/StatsPanel.tsx
@@ -89,6 +89,7 @@ export function StatsPanel({
                 stat !== "hp" &&
                 stat !== "mp" &&
                 stat !== "g" &&
+                stat !== "s" &&
                 stat !== "tier" &&
                 typeof value === "number"
             );
